358c0df18193dec44729d671ba26ccdc5b6e2a67
textcube <textcube@nate.com>
2014-03-11 10:31:14 +0900

    ext

--- VideoCrawler/crawler.sh
+++ VideoCrawler/crawler.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/php
-<?php
-$url_base = "http://d28xe8vt774jo5.cloudfront.net/abilities/videos/";
-for ($i=1;$i<150;$i++) {
-      for ($j=1;$j<6;$j++) {
-            $url = $url_base . sprintf("%04d_%02d.mp4", $i, $j);
-            //echo $url . "\n";
-            exec ("wget " . $url);
-      }
-}
+#!/bin/sh
+BASEURL=http://d28xe8vt774jo5.cloudfront.net/abilities/videos/
+for ((i=1;i<150;i++)); do
+for ((j=1;j<6;j++)); do
+URL=$BASEURL`printf "%04d_%02d.mp4" $i $j`
+#echo $URL
+/usr/bin/wget $URL
+done
+done
